4c1d841c9e1e31b72177d35faed1d17d2b50c356
Levsha765 <l.kusyj@uis.com>
2019-02-17 15:19:11 +0300

    Создание проекта

---
+++ fixture/application.py
@@ -0,0 +1,12 @@
+from pywinauto.application import Application as WinApplication
+
+
+class Application:
+
+    def __init__(self, target):
+        self.application = WinApplication(backend="win32").start(target)
+
+
+
+    def destroy(self):
+        self.application.window(title="Free Address Book").close()
